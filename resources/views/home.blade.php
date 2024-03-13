e7863d133d07a90a4e00c102c557729778ef9836
salvoPernicano <salvo.pernicano@gmail.com>
2024-03-13 20:52:23 +0100

    feat main container navigation / fix scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,41 @@
         </div>
         @endforeach
     </div>
+    <button class="load">LOAD MORE</button>
 </main>
+<section class="mainNavigator">
+    <nav>
+        <div>
+            <figure class="mainNavigatorImg">
+                <img  src="{{ Vite::asset('/resources/images/buy-comics-digital-comics.png') }}" alt="logo">
+            </figure>
+            <span>digital comics</span>
+        </div>
+        <div>
+            <figure class="mainNavigatorImg">
+                <img  src="{{ Vite::asset('resources/images/buy-comics-merchandise.png') }}" alt="logo">
+            </figure>
+            <span>dc merchandise</span>
+        </div>
+        <div>
+            <figure class="mainNavigatorImg">
+                <img  src="{{ Vite::asset('resources/images/buy-comics-subscriptions.png') }}" alt="logo">
+            </figure>
+            <span>subscription</span>
+        </div>
+        <div>
+            <figure class="mainNavigatorImg">
+                <img  src="{{ Vite::asset('resources/images/buy-comics-shop-locator.png') }}" alt="logo">
+            </figure>
+            <span>comic shop location</span>
+        </div>
+        <div>
+            <figure class="mainNavigatorImg">
+                <img  src="{{ Vite::asset('resources/images/buy-dc-power-visa.svg') }}" alt="logo">
+            </figure>
+            <span>dc power visa</span>
+        </div>
+    </nav>
+</section>
 
 @endsection
